1148c1428ef553918eebd03c49d17454f84f9189
finder <finder@finders-MacBook-Air.local>
2022-09-20 14:48:24 +0500

    Add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'css/site'
+import 'jquery'
+import('src/plugins')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("channels")
