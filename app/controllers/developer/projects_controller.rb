7496ec09540d2b20487983cdf9d2bfbfb970978d
Jawad Ahmed <jawad.ahmed@rstacklabs.com>
2022-10-04 14:44:00 +0500

    create controller and views of project in developer

--- app/controllers/developer/projects_controller.rb
+++ app/controllers/developer/projects_controller.rb
@@ -1,15 +1,18 @@
 class Developer::ProjectsController < ApplicationController
   before_action :validate_developer
+    before_action :set_project, only: :show
 
   def index
     @projects = current_user.projects
   end
 
-  def show
-    @project = Project.find(params[:id])
-  end
+  def show; end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
   def validate_developer
     return if current_user&.developer?
