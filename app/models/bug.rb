528274ffeb45ae660a034e6300227dee18009f9a
Jawad Ahmed <jawad.ahmed@rstacklabs.com>
2022-10-07 09:42:41 +0500

    add enum(bug_type/status

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -3,6 +3,9 @@ class Bug < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :solver, class_name: 'User', foreign_key: 'solver_id', optional: true
 
+  enum bug_type: {feature: 'feature', bug: 'bug'}
+  enum status: {pending: 'pending', started: 'started', completed: 'completed', resolved: 'resolved'}
+
   has_one_attached :screnshot
 
   validates :screnshot, content_type: [:gif, :png]
