0545acf00cc7ce380b861cec2d048df4bef11373
Jawad Ahmed <jawad.ahmed@rstacklabs.com>
2022-10-11 12:04:20 +0500

    Refactoring

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -1,7 +1,18 @@
 module BugsHelper
-
   def select_assigned_qa_projects
     current_user.projects.collect { |p| [p.title, p.id] }
+  end
+
+  def bug_label(bug)
+    "#{bug.solver_id? ? 'Remove' : 'Add'} Developer"
+  end
+
+  def can_assign_bug?(bug)
+    current_user.id == bug.solver_id || bug.solver_id.blank?
+  end
+
+  def project_id_exist?
+    params[:project_id].to_i.in? current_user.project_ids.to_a
   end
 end
 
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,30 +1,32 @@
 $( document ).ready(function() {
-    $( '.abc' ).hide();
+  $('.status_type' ).hide();
 });
+
 var series = [
-{name: 'feature', status: 'pending'},
-{name: 'feature', status: 'started'},
-{name: 'feature', status: 'completed'},
-{name: 'bug', status: 'pending'},
-{name: 'bug', status: 'started'},
-{name: 'bug', status: 'resolved'},
+  {name: 'feature', status: 'pending'},
+  {name: 'feature', status: 'started'},
+  {name: 'feature', status: 'completed'},
+  {name: 'bug', status: 'pending'},
+  {name: 'bug', status: 'started'},
+  {name: 'bug', status: 'resolved'},
 ]
 
 $(".bugtype").change(function(){
-    var bugtype = $('.bugtype option:selected').val();
-    if (bugtype == ''){
-      $('.abc').hide();
-    }
-    else{
-      $('.abc').show()
-    var options =  '<option value=""><strong></strong></option>'; //create your "title" option
-    $(series).each(function(index, value){ //loop through your elements
-        if(value.name == bugtype){ //check the company
-            options += '<option value="'+value.status+'">'+value.status+'</option>'; //add the option element as a string
-        }
+  var bugtype = $('.bugtype option:selected').val();
+
+  if (bugtype == ''){
+    $('.status_type').hide();
+  }
+  else{
+    $('.status_type').show()
+
+  var options =  '<option value=""><strong></strong></option>';
+  $(series).each(function(index, value){
+    if(value.name == bugtype){
+      options += '<option value="'+value.status+'">'+value.status+'</option>';
+      }
     });
-    }
+  }
 
-
-    $('.statustype').html(options); //replace the selection's html with the new options
+  $('.statustype').html(options);
 });
